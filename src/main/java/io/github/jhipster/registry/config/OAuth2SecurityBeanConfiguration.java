ee7bda3ec1b9b4141cdd36a17ef1263ad9a27372
Mr. No <no@tiger.cx>
2019-11-06 12:06:13 +0100

    tidy up OAuth2SecurityBeanConfiguration

--- src/main/java/io/github/jhipster/registry/config/OAuth2SecurityBeanConfiguration.java
+++ src/main/java/io/github/jhipster/registry/config/OAuth2SecurityBeanConfiguration.java
@@ -66,15 +66,6 @@ public class OAuth2SecurityBeanConfiguration {
         return new RegisterSessionAuthenticationStrategy(sessionRegistry);
     }
 
-    /*
-    @Bean
-    public OAuth2AuthorizedClientRepository oAuth2AuthorizedClientService(OAuth2AuthorizedClientService oAuth2AuthorizedClientService) {
-        AuthenticatedPrincipalOAuth2AuthorizedClientRepository result = new AuthenticatedPrincipalOAuth2AuthorizedClientRepository(
-            oAuth2AuthorizedClientService);
-        return result;
-    }
-     */
-
     @Bean
     public OAuth2LoginAuthenticationFilter oAuth2LoginAuthenticationFilter(
         ClientRegistrationRepository clientRegistrationRepository, OAuth2AuthorizedClientService authorizedClientService,
@@ -107,29 +98,6 @@ public class OAuth2SecurityBeanConfiguration {
         return result;
     }
 
-    /*
-    @Bean
-    public OAuth2LoginAuthenticationProvider oAuth2LoginAuthenticationProvider(
-        OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> accessTokenResponseClient,
-        KeycloakOidcUserService userService)
-    {
-        OAuth2LoginAuthenticationProvider result = new OAuth2LoginAuthenticationProvider(accessTokenResponseClient,
-            userService);
-        return result;
-    }
-     */
-
-    /*
-    @Bean
-    public OAuth2AuthorizationCodeAuthenticationProvider oAuth2AuthorizationCodeAuthenticationProvider(
-        OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> oAuth2AccessTokenResponseClient)
-    {
-        OAuth2AuthorizationCodeAuthenticationProvider result = new OAuth2AuthorizationCodeAuthenticationProvider(
-            oAuth2AccessTokenResponseClient);
-        return result;
-    }
-     */
-
     @Bean
     public AuthenticationManager authenticationManager(OidcAuthorizationCodeAuthenticationProvider p0) {
         // Potentially there could be more AuthenticationProviders...
@@ -138,27 +106,10 @@ public class OAuth2SecurityBeanConfiguration {
         return result;
     }
 
-    /*
-    @Bean
-    public OAuth2AuthorizationCodeGrantFilter oAuth2AuthorizationCodeGrantFilter(
-        ClientRegistrationRepository clientRegistrationRepository, OAuth2AuthorizedClientRepository authorizedClientRepository,
-        AuthenticationManager authenticationManager)
-    {
-        OAuth2AuthorizationCodeGrantFilter result = new OAuth2AuthorizationCodeGrantFilter(clientRegistrationRepository,
-            authorizedClientRepository, authenticationManager);
-        return result;
-    }
-     */
-
     @Bean
     public KeycloakOidcUserService keycloakOidcUserService() {
 
         KeycloakOidcUserService result = new KeycloakOidcUserService();
-        // now automatically set in @PostConstruct
-        // result.setJwtDecoder(jwtDecoder);
-        // result.setAuthoritiesMapper(authoritiesMapper);
-        // result.setKeycloakOidcUserService(keycloakOidcUserService);
-        // result.setClaimsToAuthoritiesMapper(new KeycloakClaimsToAuthoritiesMapper());
         return result;
     }
 
@@ -168,118 +119,6 @@ public class OAuth2SecurityBeanConfiguration {
         result.setRestTemplate(new RestTemplate());
         return result;
     }
-
-    /*
-    @Bean
-    public OAuth2ClientAuthenticationProcessingFilter ssoFilter(
-        OAuth2ProtectedResourceDetails details, OAuth2ClientContext oauth2ClientContext,
-        ResourceServerTokenServices tokenServices)
-    {
-        OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter(
-            "/login/keycloak");
-        OAuth2RestTemplate keycloak = new OAuth2RestTemplate(details, oauth2ClientContext);
-        filter.setRestTemplate(keycloak);
-        filter.setTokenServices(tokenServices);
-        return filter;
-    }
-     */
-
-    /*
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository(ClientRegistration clientRegistration) {
-        return new InMemoryClientRegistrationRepository(clientRegistration);
-    }
-
-    @Bean
-    public AuthorizationRequestRepository<OAuth2AuthorizationRequest> authorizationRequestRepository() {
-        return new HttpSessionOAuth2AuthorizationRequestRepository();
-    }
-     */
-
-    /*
-    @Bean
-    @ConfigurationProperties("security.oauth2.client")
-    public OAuth2ProtectedResourceDetails oAuth2ProtectedResourceDetails()
-    {
-        return new AuthorizationCodeResourceDetails();
-    }
-
-    @Primary
-    @Bean
-    @ConfigurationProperties("security.oauth2.resource")
-    public OAuth2ResourceServerProperties oAuth2ResourceServerProperties() {
-        return new OAuth2ResourceServerProperties();
-    }
-
-    @Bean
-    public OAuth2RestOperations oAuth2RestOperations(OAuth2ProtectedResourceDetails details, OAuth2ClientContext oauth2ClientContext) {
-        return new OAuth2RestTemplate(details, oauth2ClientContext);
-    }
-
-    @Bean
-    public AccessTokenProvider accessTokenProvider() {
-        return new AuthorizationCodeAccessTokenProvider();
-    }
-
-    // ??? ClientDetails are not filled-in automatically, i.e. there is something missing...
-    @Bean
-    public ClientDetails clientDetails() {
-        return new BaseClientDetails();
-    }
-     */
-
-    /*
-    @Bean
-    public OAuth2AuthenticationManager oAuth2AuthenticationManager(ResourceServerTokenServices tokenServices) {
-        OAuth2AuthenticationManager result = new OAuth2AuthenticationManager();
-        result.setTokenServices(tokenServices);
-        return result;
-    }
-     */
-
-    /*
-    @Bean
-    public AuthoritiesExtractor authoritiesExtractor() {
-        return new AuthoritiesExtractor() {
-
-            @Override
-            public List<GrantedAuthority> extractAuthorities(Map<String, Object> map) {
-                Set<GrantedAuthority> result = new HashSet<>();
-                extract(result, "roles", map);
-                extract(result, "roles2", map);
-                extract(result, "groups", map);
-
-                return new ArrayList<>(result);
-            }
-
-            private void extract(Set<GrantedAuthority> result, String key, Map<String, Object> map) {
-                Object value = map.get(key);
-                if (value instanceof Collection) {
-                    Collection<String> list = (Collection<String>) value;
-                    list.stream()
-                        .filter(s -> !StringUtils.isEmpty(s))
-                        .map(s -> new SimpleGrantedAuthority(s.trim()))
-                        .forEach(result::add);
-                } else if (value instanceof String) {
-                    result.add(new SimpleGrantedAuthority((String) ((String) value).trim()));
-                } else {
-                    LOG.warn("Could not map value: " + value);
-                }
-            }
-        };
-    }
-     */
-
-    // stackoverflown: Handle UserRedirectRequiredException
-    /*
-    @Bean
-    public FilterRegistrationBean<OAuth2ClientContextFilter> oauth2FilterRegistration(OAuth2ClientContextFilter filter) {
-        FilterRegistrationBean<OAuth2ClientContextFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(filter);
-        registrationBean.setOrder(-100);
-        return registrationBean;
-    }
-     */
 
     @Bean
     @SuppressWarnings("unchecked")
